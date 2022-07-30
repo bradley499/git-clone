a92826a5516c3e9b4c52bad14079cffc8e76fd21
bradley499 <bradm499@gmail.com>
2022-07-30 12:36:38 +0100

    Created git-clone

---
+++ git-clone.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Automatically git clone https connection to ssh
+
+if [[ $# -eq 0 ]]; then
+	exit 0
+fi
+
+if [[ $1 == "https://github.com/*" ]]; then
+    git clone git@github.com:${1:19}
+else
+    git clone $1
+fi
